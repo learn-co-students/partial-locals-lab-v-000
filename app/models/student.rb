c7968e97ebeb4f565a55099b7a041dd8b7e5f745
frankNowinski <nowinski.frank@gmail.com>
2016-01-27 05:12:47 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,14 @@
-# == Schema Information
-#
-# Table name: students
-#
-#  id         :integer          not null, primary key
-#  name       :string
-#  hometown   :string
-#  birthday   :datetime
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    if name.present?
+      all.collect do |student|
+        student if student.name.downcase.include?(name)
+      end.compact
+    else
+      self.all
+    end
+  end
 end
