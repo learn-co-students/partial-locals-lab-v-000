5603df695d6f73f29beb39f157d0bd116ba21a52
Brett <brettrheenan@gmail.com>
2016-02-20 16:11:11 +0100

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+  	where('name like ?', "%#{query}%")
+  	# q = query.downcase
+  	# self.all.select do |s|		
+  	# 	/#{q}/.match(s.name.downcase)
+  	# end
+  end
+
 end
