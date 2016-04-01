50cde8be4401fa852b021dc1652fd9b018b31918
Smulligan85 <sean.mulligan85@gmail.com>
2016-04-01 07:07:45 -0700

    Done

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,16 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
+require 'pry'
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    if name.present?
+      self.all.collect {|s| s if s.name.downcase.include?(name)}.compact
+    else
+      self.all
+    end
+  end
 end
