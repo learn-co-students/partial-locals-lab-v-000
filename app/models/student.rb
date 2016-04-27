cc5da40324cc8444a49b923e2adedce09bc8916d
Gandhi Anderson <thegands@gmail.com>
2016-04-26 23:07:36 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query_string)
+    if query_string.blank?
+      self.all
+    else
+      self.where("name like ?", "%#{query_string}%")
+    end
+  end
 end
