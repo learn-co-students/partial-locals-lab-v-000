e474a7cf456f88a6b0adcb05562af12bd22b3f56
glee38 <glee3388@gmail.com>
2016-04-29 18:45:10 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,27 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  # this method works:
+
+  # def self.search(student)
+  #   @students = []
+  #   if student.blank?
+  #     self.all
+  #   else
+  #     self.all.map do |s|
+  #       if s.name.downcase.include?(student.downcase)
+  #         @students << s
+  #       end
+  #     end
+  #     @students
+  #   end
+  # end
+
+  # this method is better:
+  def self.search(search)
+    # example: SELECT "students".* FROM "students" WHERE (name LIKE '%cris%')
+    self.where("name LIKE ?", "%#{search}%") 
+  end
+
 end
