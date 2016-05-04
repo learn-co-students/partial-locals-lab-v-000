5184b61fd078e4fc97af4042dffc4e6fbfa019c4
kylegraydev <kylegraydev@gmail.com>
2016-05-04 13:49:54 -0700

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search_terms)
+  	if search_terms.present?
+  		Student.all.select {|student| student.name.downcase.include?(search_terms)}
+  	else
+  		self.all
+  	end
+  end
+
+
 end
