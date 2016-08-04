7aff1fc622b7ac9522fc6df339dc66da81833525
jbok4 <jbok4@aol.com>
2016-08-04 16:15:35 -0400

    complete with search window

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,6 +22,11 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
+    if params[:search]
+      @students = Student.search(params[:search]).order("created_at DESC")
+    else
+      @students = Student.all.order('created_at DESC')
+    end
   end
 
   def student_params
