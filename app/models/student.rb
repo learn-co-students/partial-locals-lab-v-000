f65a807c99a57f0cc8551869f54f8e66c505379c
Jason <jasonkwong11@gmail.com>
2016-08-06 22:55:36 -0400

    done

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,17 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
+require 'pry'
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  validates_presence_of :name, :hometown, :birthday
+  
+#returns array with matching students by name
+def self.search(name_input)
+  self.all unless name_input.present?
+  where(['NAME like ?', "%#{name_input}%"])
+end
+
+
 end
