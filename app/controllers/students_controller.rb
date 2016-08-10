57e5f0d0c6d960f07e914afb6c23840cf976574a
cmiller36 <m.calliemiller@gmail.com>
2016-08-10 13:21:26 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,6 +22,7 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
+    @students = Student.search(params[:search]) if params[:search].present?
   end
 
   def student_params
