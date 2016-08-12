cec29405dff58c082b4c88fba8aa816d870307ef
arturusfury <bri.reynolds@gmail.com>
2016-08-11 19:20:05 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,17 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  validates_presence_of :name, :birthday, :hometown
+
+  def self.search(query)
+    if query.present?
+      where('NAME like ?', "%#{query}%")
+    else
+      all
+    end
+  end
 end
