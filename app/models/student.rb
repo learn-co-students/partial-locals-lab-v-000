2d66b87c2f61af17f1a9c89397f8115c404aa5af
Lenn Hypolite <lenntriniman@hotmail.com>
2016-10-11 05:58:58 +0000

    Done

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    answer =  Student.find_by_sql("SELECT * FROM Students WHERE Name LIKE '%#{name}%'")
+    if name != nil
+      answer
+    else
+      Student.all
+    end
+  end
+
 end
