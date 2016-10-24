f5daee03777512acb11f102e193c14d932f46af6
Ivan Pilot <ivanpilot@gmail.com>
2016-10-24 01:51:45 +0100

    all tests passed

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,11 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+      self.all.select do |student|
+        !student.name.scan(/#{name}/i).empty?
+      end
+  end
+
 end
