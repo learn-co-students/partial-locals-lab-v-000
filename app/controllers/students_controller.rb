0c1b3817596f821cbbce3f7b79cb7106a640886c
bmolina-nyc <bruce.d.molina@gmail.com>
2016-11-10 23:33:47 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,7 @@ class StudentsController < ApplicationController
   end
 
   def index
-     @students = Student.search(params[:query])
+    @students = Student.search(params[:keyword])
   end
 
   def student_params
