e3600121587404a4aa27fed1d5cc63d74cf88a5b
Clare Horton <clare.e.horton@gmail.com>
2016-11-13 02:10:50 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    if name.present?
+      self.all.select do |student|
+        student.name.downcase.include?(name.downcase)
+      end
+    else
+      self.all
+    end
+  end
 end
