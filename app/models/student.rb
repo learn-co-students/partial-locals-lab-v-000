bb8f6324be07de725bec4a50c8d347277043a76b
Bill <storywr@gmail.com>
2017-01-14 12:47:59 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,27 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(string)
+    name_array = []
+    if string == ""
+      all
+    else
+      all.each do |student|
+        if student.name.downcase.include?(string)
+          name_array << student
+        end
+      end
+      name_array
+    end
+  end
+
+
+
+
+
+
+
+
+
 end
