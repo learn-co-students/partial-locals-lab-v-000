e090ed2a5cc44b793459ed4a4e41424a2bcb56f3
Adam Lucia <lucia.adam@gmail.com>
2017-02-07 23:19:03 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,17 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(term)
+    results = []
+    if term == ""
+      results = self.all
+    else
+      dterm = term.downcase
+      self.all.each do |student|
+        results << student if student.name.downcase.include?(dterm)
+      end
+    end
+    results
+  end
 end
