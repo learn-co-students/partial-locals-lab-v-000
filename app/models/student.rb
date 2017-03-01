43edfe1b9700ad6185d078efe75ba5bc624fa020
Anna Lansfjord <lansfjord@gmail.com>
2017-03-01 23:05:09 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,7 +10,19 @@
 #  updated_at :datetime         not null
 #
 
+
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  validates :name, presence: true
+  validates :birthday, presence: true
+
+  def self.search(name)
+    if name
+      where('name LIKE ?', "%#{name}%")
+    else
+     all
+    end
+  end
+
 end
