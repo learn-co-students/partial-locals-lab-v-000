a87086275a0755754e814278b3b8bbdce5866856
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-10 00:17:33 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,11 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search)
+    @search = search
+    name.present? ? Student.where("name like ?", "%#{@search}%").all : Student.all
+  end
+
+
 end
