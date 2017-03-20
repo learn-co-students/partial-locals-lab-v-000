3e803d9392f221512d1f359fb8b6feee93d7e28f
Kevin Adkins <kevinladkins@gmail.com>
2017-03-20 16:22:21 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,13 @@
 class StudentsController < ApplicationController
+
+  def index
+    if params[:search]
+      @students = Student.search(params[:search])
+    else
+      @students = Student.all
+    end
+  end
+
   def new
     @student = Student.new
   end
