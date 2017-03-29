bede5b1b5449e64090c1f6270d3a98389cc8bdb2
github username pianistps <github email address pianistps@yahoo.com>
2017-03-29 15:11:38 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,20 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(arg)
+    students = []
+    if arg == ''
+      Student.all.each do |s|
+        s
+      end
+    else
+      Student.all.each do |s|
+        if s.name.match(/(?i)(#{arg})/)
+          students << s
+        end
+      end
+      students
+    end
+  end
 end
