6a372849399aee71d5de16ea8d52eae027a428e1
Alex Glover <Alex@MacBook-Pro-3.local>
2017-03-30 11:15:03 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,9 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    # binding.pry
+    self.all.select{|x| x.name.downcase.include?(name.to_s.downcase)}
+  end
 end
