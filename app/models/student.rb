29b18e1ce857fdd2a1854b5ce90adcba663b6fec
Howard DeVennish <howarddevennish@gmail.com>
2017-05-02 14:05:22 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,8 +14,11 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def self.search(name = '')
-    return Student.all if name == ''
-    Student.all.select {|student| student if student.name.downcase.include?(name.downcase)}
+  def self.search(name)
+    if name
+      Student.all.select {|student| student if student.name.downcase.include?(name.downcase)}
+    else
+      Student.all
+    end
   end
 end
