0f742715dca2c0cefc440aa8d45f3cda96f5fe9b
Zenia <zeniavilla@hotmail.com>
2017-05-23 10:07:09 -0700

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(student)
+    if student.empty?
+      @students = Student.all
+    else
+      @students = self.select {|s| s.name.downcase.include?(student.downcase)}.flatten
+    end
+    @students
+  end
+  
 end
