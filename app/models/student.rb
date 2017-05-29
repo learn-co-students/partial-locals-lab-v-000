617d08cc2c9fbd53d01bd6af2402ae86cc4d8c5a
kromoser <kevinromoser@gmail.com>
2017-05-29 12:21:51 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search_terms)
+    if search_terms == ""
+      Student.all
+    else
+      Student.all.select do |student|
+        student.name.match(/(#{search_terms})/i)
+      end
+    end
+
+  end
 end
