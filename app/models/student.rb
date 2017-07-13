7d7b523caaef0c40b84a648921c0ad4f5235517d
Allison Chesky <afc340@nyu.edu>
2017-07-13 11:59:13 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    array = []
+    if name == ""
+      array = self.all
+    else
+      array = self.all.select {|x| x[:name].include?(name.capitalize)}
+    end
+    array
+  end
 end
