cc86b6bf770a17204ec970dbae827cb0ff927920
curlywallst <nnoyes@comcast.net>
2017-07-19 10:12:21 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(str)
+    if str != ""
+      Student.all.select { |s| s.name.downcase.include? str}
+    else
+      Student.all
+    end
+  end
+
 end
