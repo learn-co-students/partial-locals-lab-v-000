ce42e9c5f90d05229f59a1debbbf518769b8ccdf
Jack <jackexyou@gmail.com>
2017-09-25 02:07:02 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(x)
+    if x == ""
+      all = self.all
+    else
+      self.where('name LIKE ?', "%#{x}%").all
+    end
+  end
 end
