0515fac3fda46af5b825fe72f8a156d97c66f07f
mikka <mikkanthrope@gmail.com>
2017-10-03 11:36:11 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,10 +21,15 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    if params[:name].present?
+     @students = Student.search(params[:name])
+     # SELECT * FROM students WHERE name LIKE %rob%
+   else
+     @students = Student.all
+   end
   end
 
   def student_params
-    params.require(:student).permit(:name, :birthday, :hometown)
+    params.require(:student).permit(:name, :birthday, :hometown, :query)
   end
 end
