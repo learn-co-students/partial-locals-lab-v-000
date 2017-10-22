a60ca2dd226d9c0e3785de408d5bbefa39280f2d
burrahey <burra.archana@gmail.com>
2017-10-22 16:03:57 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -24,6 +24,11 @@ class StudentsController < ApplicationController
     @students = Student.search
   end
 
+  def search
+    @students = Student.search(params[:search])
+    render 'index'
+  end
+
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
   end
