2ff987b49ba91d53cfa381da5d114408acae3d8b
Joanna <jmgsese@gmail.com>
2017-10-31 18:52:37 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,29 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  # def self.search(n)
+  #   Student.all.map do |student|
+  #     if student.name.downcase.include?(n.downcase)
+  #       student.name
+  #     elsif n == ""
+  #       Student.all
+  #     end
+  #   end
+  # end
+
+  # def self.search(query)
+  #   # Student.all.select do |student|
+  #   #   student.name.downcase.include?(searched_name.downcase)
+  #   #
+  #   # end
+  #   # if searched_name.empty?
+  #   #   return Student.all
+  #   # end
+  # end
+
+  def self.search(query)
+   return Student.all if query.empty?
+   search_results = Student.all.select {|student| student.name.downcase.include?(query.downcase)}
+  end
 end
