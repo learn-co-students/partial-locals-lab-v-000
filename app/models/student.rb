38bbfc5f66cea860abe557180390d947d93a1077
Justin Daniel <justinddaniel@gmail.com>
2017-11-20 22:51:49 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,7 +10,23 @@
 #  updated_at :datetime         not null
 #
 
+require 'pry'
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(student_name = nil)
+    search_results = []
+    if student_name == nil
+      Student.all
+    else
+      Student.all.each do |s|
+        if s.name.downcase.include?(student_name)
+          search_results << s
+        end
+      end
+      search_results
+    end
+  end
+
 end
