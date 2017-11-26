cde265236f93b04241e87c6627f15a6d39e0a6fb
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-26 18:48:48 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(term)
+    if term == ""
+      self.all
+    else
+      Student.all.select {|person| person.name.include?(term.titleize) }
+    end
+  end
 end
