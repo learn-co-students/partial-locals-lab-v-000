f28b34e0d0ab40ecbf80af055435577e0f0bd880
Igor Eskin <eskinhd@gmail.com>
2017-11-28 01:50:30 +0000

    Done.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,13 @@
 class ClassroomsController < ApplicationController
+
   def show
     @classroom = Classroom.find(params[:id])
     @student = @classroom.oldest_student
+    # @students = @classroom.students
   end
 
   def index
     @classrooms = Classroom.all
   end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -11,6 +11,13 @@
 #
 
 class Student < ActiveRecord::Base
+
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(string)
+    self.all.select { |student| student.name.match(/(#{string})/i) }
+  end
+
+
 end
