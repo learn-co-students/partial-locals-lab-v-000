f4a382fe5bba0df20ed6c35b22c3a7b99e8f9224
Dasha Nikitina <dashadariallc@gmail.com>
2017-12-06 09:19:14 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -23,11 +23,11 @@ class StudentsController < ApplicationController
 
   def index
   @students = Student.all
-    if params[:search]
-      @students = Student.search(params[:search]).order("created_at DESC")
-      else
-      @students = Student.all.order("created_at DESC")
-    end
+    # if params[:search]
+    #   @students = Student.search(params[:search]).order("created_at DESC")
+    #   else
+    #   @students = Student.all.order("created_at DESC")
+    # end
   end
 
 
