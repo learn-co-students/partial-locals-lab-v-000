879c7a9e85634c0eac281531f7ade2e67ef47cce
chrisjamesr <chrisjames.r@gmail.com>
2017-12-06 22:54:24 -0500

    passing all tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+  def self.search(student_name)
+    if student_name.blank?
+      Student.all
+    else
+      @students = Student.all.where("name LIKE ? OR ?", "%#{student_name.downcase}%", "#{student_name.capitalize}%")
+    end
+  end
+  
 end
