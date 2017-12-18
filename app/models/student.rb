685217ff4be5ba2b7e4d6d67f4fcad41ace0656b
github username <github email address>
2017-12-17 21:17:50 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,17 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  validates :name, presence: true
+  validates :hometown, presence: true
+  validates :birthday, presence: true
+
+
+  def self.search(name_query)
+    if name_query.present?
+      where("name LIKE ?", "%#{name_query}%")
+    else
+      self.all
+    end
+  end
 end
