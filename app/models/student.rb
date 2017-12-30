095594ade13184f083b409deaed5b422f31c9f12
Tavish Harrison <creationmargin26@gmail.com>
2017-12-30 19:14:48 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,12 @@
-# == Schema Information
-#
-# Table name: students
-#
-#  id         :integer          not null, primary key
-#  name       :string
-#  hometown   :string
-#  birthday   :datetime
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search)
+    if search.present?
+      where('NAME like ?', "%#{search}%")
+    else
+      self.all
+    end
+  end
 end
