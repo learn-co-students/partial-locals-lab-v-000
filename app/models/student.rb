6c25a904b9c1dafd247a08e693baa5f2db9e5051
dbfarms <matt.hunger@gmail.com>
2017-12-31 21:41:28 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,22 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  
+  def self.search(query) 
+    search_result = []
+    Student.all.each do |student|
+      if student.name.downcase.include?(query)
+        search_result << student 
+      end 
+    end 
+    
+    
+    #binding.pry
+    if search_result == [] 
+      return Student.all
+    else 
+      return search_result
+    end 
+  end 
+  
 end
