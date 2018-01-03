ad60761d8c46ff7e55f3750428bc69edb76e7b06
EC2 Default User <ec2-user@ip-172-31-22-232.us-east-2.compute.internal>
2018-01-03 19:37:02 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,17 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  
+  def self.search(string) 
+    student_name = []
+     self.all.each do |student|
+       if student.name.downcase.include?(string) 
+         student_name << student
+      else
+         self.all
+      end
+    end
+    student_name
+  end
+   
 end
