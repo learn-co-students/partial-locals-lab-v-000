426360db4f18e673be819f36ca5811c89be67a28
Scott Wakeman <github email address>
2018-02-09 09:49:40 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,16 +21,9 @@ class StudentsController < ApplicationController
   end
 
   def index
-
   #Add in search functionality such that users can search for a student by name
   #and see all matching results on the students index page.
-    #@students = Student.all
-    #@students = Student.all.count
-    #@students = Student.search.count
-    #@students = Student.search.all.count
     @students = Student.search(params[:query_name])
-
-      #students/_student.html.erb
   end
 
   def student_params
