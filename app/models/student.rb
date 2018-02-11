b31304a0fc278a5c164698cd0d3d95605c04f60c
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-02-11 11:32:53 -0500

    Tests passing

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query.present?
+      Student.all.select {|student|student.name.downcase.include?(query.downcase)}
+    else
+      self.all
+    end
+  end
+
+
+
 end
