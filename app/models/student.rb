6b1157111c138fdf4039688e341e4bae2332d3a8
PJ Frias <peejfrias@gmail.com>
2018-02-25 01:49:47 +0000

    save

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,9 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    self.all.collect{|student| student.include?(name)}
+  end
+
 end
