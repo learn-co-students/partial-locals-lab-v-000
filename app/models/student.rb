bc1c6a3134273ad0dc20bf4219c292b633f354b6
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-04 21:12:03 +0100

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(arg)
+    if !arg.blank?
+      Student.all.select { |s| s.name.downcase.include?(arg)}
+    else
+      Student.all
+    end
+
+  end
 end
