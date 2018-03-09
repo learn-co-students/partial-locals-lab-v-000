833205118beac527d20a98adf9a5619f57e0f78e
rh24 <ms.rebeccahong@gmail.com>
2018-03-09 17:28:49 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,9 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    name != '' ? self.all.select { |s| s.name if s.name.downcase.include?(name) } : self.all
+      # why doesn't .map work and .select does?
+  end
 end
