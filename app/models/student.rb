6632b17531b409286028a7095a46d368d52f3def
ronald vilorio <soccer23415@gmail.com>
2018-04-11 19:36:59 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,19 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
-end
+
+  def self.search(student)
+    # binding.pry
+    if student.empty?
+      self.all
+    else
+      self.all.map do |s|
+          if s.name.match((student.capitalize)) || s.name.match((student))
+            s
+          end
+      end.compact #do
+
+    end #if
+
+  end #method
+end #class
