9eaa2df2d2dc9b80d15f3490b1c611c7f6c72cce
badlychadly <chadmontoya21@yahoo.com>
2018-04-18 20:24:16 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,7 +15,7 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   def self.search(name)
-    if name.empty?
+    if name.nil? || name.empty?
       self.all
     else 
       self.all.select do |student| 
