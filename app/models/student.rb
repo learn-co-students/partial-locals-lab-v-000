fc6f5072c8ccba13f3fe7783ece4b3e4e2850185
anthonygharvey <anthony@anthonygharvey.com>
2018-04-24 10:39:18 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  
+  def self.search(pattern)
+    if pattern.present? 
+      where('name LIKE ?', "%#{pattern}%")
+    else
+      all
+    end
+  end  
 end
