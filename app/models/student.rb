74357a72f57bd3992b48e46478d5829e7c279e64
github username <github email address>
2018-06-05 10:01:48 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search_query)
+    if search_query.present?
+      where('NAME like ?', "%#{search_query}%")
+    else
+      self.all
+    end
+  end
 end
+
+# NAME? Does capitalization matter? like? Why is like used at all? How can I see the SQL involved?
