2cfb5d9493608034262ff501b6630fea3f5d9bfd
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-14 19:20:20 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  
+  def self.search(argument)
+    array = self.all.collect do |student|
+      student if student.name.downcase.include?(argument.downcase)
+    end
+    array.compact
+  end
+
 end
