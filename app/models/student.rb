3cbd6f4e2e5016a4d3088b216a12393fc431fd32
Gingertonic <bethmschofield@gmail.com>
2018-06-18 19:34:51 +0930

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,17 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search)
+    @results = []
+    if search.blank?
+      @results = Student.all
+    else
+      Student.all.each do |student|
+        @results << student if student.name.downcase.match(search.downcase)
+      end
+    end
+    @results
+  end
+
 end
