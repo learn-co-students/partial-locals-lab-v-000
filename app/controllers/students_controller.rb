3fc5d9123ed4198d500d79a45d52d789655d31a1
Joseph Parker <jparker010190@gmail.com>
2018-07-03 00:20:30 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,11 @@
 class StudentsController < ApplicationController
+  def index
+    if params[:search]
+      @students = Student.search(params[:search])
+    else
+      @students = Student.all
+    end
+  end
   def new
     @student = Student.new
   end
