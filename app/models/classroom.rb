d482eb3c93ccf20691e5d487ab14c8b9070895a0
jokerdelta <jokerdelta@yahoo.com>
2018-07-15 12:32:32 -0700

    Done.

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -17,12 +17,12 @@ class Classroom < ActiveRecord::Base
     students.where("birthday is not null").order("birthday asc").first
   end
 
-  def self.search(name)
-    if params[:name]
-      where('name LIKE ?', "%#{name}%")
-    else
-      Students.all
-    end
-  end
+  # def self.search(name)
+  #   if params[:name]
+  #     where('name LIKE ?', "%#{name}%")
+  #   else
+  #     Students.all
+  #   end
+  # end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,22 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  validates :name, presence: true
+  validates :hometown, presence: true
+  validates :birthday, presence: true
+
+
+
+
+  def self.search(name)
+    if name.present?
+      where('NAME like ?', "%#{name}%")
+    else
+      self.all
+    end
+
+  end
+
 end
+
