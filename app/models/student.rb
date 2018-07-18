735fe665bcd7acf7970ec14fb0f0b16e7713da4f
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-18 19:15:40 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,6 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
+
   def self.search(str)
     if str.present?
       Student.where('name LIKE ?', str + '%').all
