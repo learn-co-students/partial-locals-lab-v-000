700a678678a213bf0224132dfa3f2b0ee7dae46c
Suchitra Naik <susanaik@gmail.com>
2018-08-07 15:59:04 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query_name)
+    if name.present?
+      where('NAME like ?', "%#{query_name}%")
+    else
+      self.all
+    end
+  end
+
 end
