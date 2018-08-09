1fcb1085d75667fd3c3c0b1affecbb7153f3d2bd
Jayon Kim <jpkim921@gmail.com>
2018-08-09 02:04:41 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,22 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+  def self.search(name_to_search)
+
+    matches = Student.all.select do |student|
+      student.name.downcase.include?(name_to_search)
+    end
+
+    # student is found return all students whose name contains the given string
+    if !matches.empty?
+      matches
+    else
+      # returns all students
+      Student.all
+    end
+
+  end
+
 end
