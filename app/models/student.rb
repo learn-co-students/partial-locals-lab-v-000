dd5940be7b4fcdb307a58cf39b184e60ef8b30e6
Katrina Brinson <kbrinso@gmail.com>
2018-08-13 22:27:21 +0000

    9 failing

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,17 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(n)
+    students = []
+    if !n.empty?
+      Student.all.each {|s|
+        if s.name.downcase.include?(n.downcase)
+          students << s
+        end
+      }
+    end
+    # Student.where(name: name)
+    students.empty? ? Student.all : students
+  end
 end
