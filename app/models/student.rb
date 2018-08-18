293f00ec624b5312e02dbc29808542eb657e177e
ElvinGarcia <dimix181@gmail.com>
2018-08-17 22:33:21 -0400

    completed student model search

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search (*arg)
+    if  arg.first.empty?
+      Student.all
+    else
+      #returns similar match to the argument passed
+    Student.where('name like ? ', "%#{arg.first}%")
+    end
+  end
+
 end
