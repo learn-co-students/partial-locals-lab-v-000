76a7b025d406b3e3a2318105294945b8b2455d68
Shirley Collins <shirley@m104.us>
2018-08-23 17:09:31 -0700

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search_query)
+    # https://stackoverflow.com/questions/22596861/how-can-i-match-a-partial-string-to-a-databases-objects-attribute-regexp
+    if !search_query.blank?
+      where('name like ?', "%#{search_query}%")
+    else
+      self.all
+    end
+  end
 end
