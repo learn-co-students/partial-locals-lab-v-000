09a0d67ec4fd15c9381291a574308fbff408856c
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-27 22:57:20 +0000

    Done.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,6 +1,11 @@
 class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
+<<<<<<< HEAD
+=======
+binding.pry    
+    # @students = Student.all
+>>>>>>> 41bfddab7b107ef168d340e936416d377e0d3da3
   end
 
   def index
