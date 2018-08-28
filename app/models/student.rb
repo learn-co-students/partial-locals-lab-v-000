42fbce028ecd4b64322206577c410ae7cfb4aaff
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-08-28 13:32:41 -0400

    pass all tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,22 +14,11 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-# the tests require that this doesn't return an array, so need to use ActiveRecord method for finding instead
-
   def self.search(query)
-binding.pry
     if query.empty?
       results = self.all
     else
-      results = self.all.collect do |student|
-        student if student.name && student.name.upcase.include?(query.upcase)
-      end
-
-      results = results.compact
-
-      if results.length == 1
-        results[0]
-      end
+      results = self.where("NAME like ?", "%#{query}%")
     end
   end
 
