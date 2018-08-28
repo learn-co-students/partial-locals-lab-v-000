320a3219de0a8f1ab519abe0d3389a58732779a1
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-28 05:35:14 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -16,7 +16,7 @@ class Student < ActiveRecord::Base
   
   def search(query)
     self.all.collect do |student|
-      /#{query}/.match?(student)
+      student.name.include? query
     end
   end
   
