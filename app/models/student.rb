835a4368933181b11118127a194e1ef9303f71df
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-08-28 04:30:30 -0400

    pass student model, work through student index tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,22 +13,24 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
-  
+
+# the tests require that this doesn't return an array, so need to use ActiveRecord method for finding instead
+
   def self.search(query)
+binding.pry
     if query.empty?
       results = self.all
     else
-      
       results = self.all.collect do |student|
-        
-        if student.name && student.name.upcase.include?(query.upcase)
-          student
-        end
-        
+        student if student.name && student.name.upcase.include?(query.upcase)
       end
-      
+
+      results = results.compact
+
+      if results.length == 1
+        results[0]
+      end
     end
-    results.compact
   end
-  
+
 end
