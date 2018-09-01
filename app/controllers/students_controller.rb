7b951f3c6fc7dee7fffb131716bca955b41dda0c
epic0448 <samuelogundiran@Yahoo.com>
2018-09-01 01:19:53 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -24,6 +24,11 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def  search
+    @students = Student.all
+
+  end
+
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
   end
