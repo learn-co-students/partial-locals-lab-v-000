2d0a0b44ddd7327f25aff652cef027c45afad4d3
Anu Khambete <anukhambete@gmail.com>
2018-10-16 23:05:48 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,22 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
+require 'pry'
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    student_array = []
+
+    if name.blank?
+      student_array = self.all
+    else
+      #student_array =  self.all.collect {|x| x.name.downcase}.reject {|x| !x.include?(name)}
+      student_array = self.all.reject{|x| !x.name.downcase.include?(name.downcase)}
+    end
+    #binding.pry
+    student_array
+  end
+
 end
