e797d11befc8404d27bacee3730a6f76aa9af76b
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-26 23:56:43 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,18 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def search(string)
+    if string == ""
+      return Student.all
+    else
+      array = []
+      all = Student.all
+      all.each do |name|
+        if name.include?(string)
+          all << string
+        end
+      end
+    end
+  end
 end
