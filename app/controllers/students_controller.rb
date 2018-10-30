02223f7515a4cfe0a91e63d81e6c94fc568a2d91
geoffrey woodcock <theatomproject@gmail.com>
2018-10-30 20:26:22 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,9 +22,11 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
+   
   end
-
+   
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
   end
+  
 end
