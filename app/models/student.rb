4e4265bdefc8112905f9e68f87e48a08b8ab078a
mattetress <mattetress@me.com>
2018-11-26 13:59:01 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+  def self.search(search_term)
+    if search_term
+      where('name LIKE ?', "%#{search_term}%")
+    else
+      self.all
+    end
+  end
+
 end
