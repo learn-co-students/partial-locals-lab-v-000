b53c9242caba4e0a545862ea7ea2b961a968a04a
jinjo39 <c.delavar@gmail.com>
2018-12-05 13:27:18 -0800

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -11,6 +11,15 @@
 #
 
 class Student < ActiveRecord::Base
+  require 'pry'
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(student)
+    if student == ""
+      self.all
+    else
+      self.all.find_all {|s| s.name.include?(student[0].upcase) }
+    end
+  end
 end
