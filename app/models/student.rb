65dbf54684c416c028b4a7537773b1c053e98e7b
Elizabeth <efkarst@outlook.com>
2019-01-02 18:56:09 -0800

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,27 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search)
+    students = []
+    Student.all.each do |student|
+      students << student if student.name.downcase.include?(search.downcase) == true
+    end
+    students
+  end
 end
+
+
+# <%= form_tag student_search_path do %>
+#   <label> Search for a student: </label>
+#   <%= text_field_tag :'search' %>
+#   <%= submit_tag 'Search' %>
+# <%end%>
+
+
+# <li>
+# Birthday: <%= student.birthday.strftime("%m/%d/%Y")  %>
+# </li>
+# <li>
+# Hometown: <%= student.hometown.capitalize %>
+# </li>
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  post 'students/student_search', to: 'students#index', as: 'student_search'
+  get 'students/student_search', to: 'students#index'
   resources :classrooms
   resources :students
+  # get 'students/student_search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
