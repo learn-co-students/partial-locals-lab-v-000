50176b41a9d0cec2bd2a7ef798dda9d52a763411
Josely Castro <joselycas@gmail.com>
2019-01-26 15:27:20 -0600

    Done.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,9 +1,7 @@
 class ClassroomsController < ApplicationController
+
   def show
     @classroom = Classroom.find(params[:id])
-    binding.pry
-    @student = Student.find(params[:id])
-
   end
 
   def index
