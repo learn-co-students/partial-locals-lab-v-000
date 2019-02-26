70e8a2bc5708bb156c943d8cead62bea24ce2d1a
James Borocz <thesilentc@me.com>
2019-02-25 22:10:06 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,24 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
 class Student < ActiveRecord::Base
-  has_many :classroom_students
-  has_many :classrooms, through: :classroom_students
+has_many :classroom_students
+has_many :classrooms, through: :classroom_students
+
+def self.search(name)
+    students = []
+    if name.empty?
+      return self.all
+    else
+        self.all.each do |student|
+        if student.name.downcase.include?(name.downcase)
+          students << student
+        end
+      end
+    end
+
+  return students
+
+end
+
 end
