f9c0cc4048a8f3cb158d537caf78e59c2b73e8de
github username <github email address>
2019-04-26 13:08:51 -0400

    Done.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,7 +1,9 @@
 class ClassroomsController < ApplicationController
   def show
+    binding.pry
     @classroom = Classroom.find(params[:id])
     @student = @classroom.oldest_student
+    @students = Student.all
   end
 
   def index
