46f25bf9c3c39c1851cf6ab9a348d7a4facdf799
Lois Lee <lois.lee@me.com>
2019-05-20 21:52:43 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,10 +21,13 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
   end
+
+  private
 
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
   end
+
 end
