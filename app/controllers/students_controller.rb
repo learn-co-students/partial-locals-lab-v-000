82e68f025d1883756f55a58863af1623ab110ae6
ahotchkin <ahotchk1@yahoo.com>
2019-06-09 13:07:56 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,8 +21,13 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
+    render 'index'
   end
+
+  # def search
+  #   @students = Student.search(params[:name])
+  # end
 
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
