5a013e1176ea69fcc1b4d93d13c584f7f8d388cd
Sushi Chanrai <sushi@pathworks.co>
2019-06-27 21:38:26 +0100

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(term)
+    if term == ''
+      self.all
+    elsif term.present?
+      where('NAME like ?', "%#{term}%")
+    end
+  end
+
+
 end
