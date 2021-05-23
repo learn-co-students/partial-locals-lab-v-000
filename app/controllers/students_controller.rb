976747e3239a41a986964c78998b6857268942a4
Lamia Haider <lamia.haider@gmail.com>
2021-05-23 11:41:44 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,11 @@
 class StudentsController < ApplicationController
+
+
+  def index
+    @students = Student.all
+  end
+
+
   def new
     @student = Student.new
   end
@@ -20,9 +27,6 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def index
-    @students = Student.all
-  end
 
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
